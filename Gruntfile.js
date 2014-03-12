f1d9211e028fe1707342edd9aea49a6a099c74b5
Sergey Bondar <msnre@yandex.ru>
2014-03-12 23:44:42 +0400

    Arch

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'build/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'build/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        qunit: {
+            files: ['test/**/*.html']
+        },
+        jshint: {
+            jshintrc: true,
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+
+            beforeconcat: ['src/**/*.js'],
+            afterconcat: ['build/<%= pkg.name %>.js']
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'qunit']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('test', ['jshint', 'qunit']);
+
+    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,44 @@
+
+Зависимости
+===========
+
+node.js+npm
+-----------
+
+Зависимости package.json
+
+TODO bower/...?
+
+grunt
+-----
+
+Таскраннер/Сборщик Grundfile.js
+
+TODO plans?
+
+bower
+-----
+
+Зависимости библиотек bower.json
+
+TODO planefight-mech, planefight-ai сделать зависимостями
+
+jshint
+------
+
+Код стайл .jshintrc
+
+https://github.com/serheyShmyg/requirejs-jquery-backbone
+
+
+imagesmin - minified all images;
+
+
+clean - file remover;
+
+less - convert all less files in one css file (default);
+
+fake server sinon.js - use for fake response on ajax request (emulation server response);
+karma - test runner
+mocha - framework for testing
+chai - lexical library for any testing framework
